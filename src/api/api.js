8d264251eb66137ddc8283f91911d56ab13777f9
piyush199311 <piyush.sharma.3101@gmail.com>
2024-03-23 14:44:13 +0530

    cloudfront api

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const HOST = "http://localhost:4000";
+const HOST = "https://dkzpedoeku2z0.cloudfront.net";
 export const LOGIN = `${HOST}/api/public/login/admin`;
 export const LIST_WITHDRAWAL = `${HOST}/api/admin/list/withdrawals`;
 export const EXPORT_WITHDRAWAL = `${HOST}/api/admin/export/withdrawals`;
