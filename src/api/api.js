eddc84a1b7c008984c9c8f5bee447b0494488794
piyush199311 <piyush.sharma.3101@gmail.com>
2024-03-26 11:15:36 +0530

    fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const HOST = "https://dkzpedoeku2z0.cloudfront.net";
+const HOST = "https://e26f-2409-4055-2e85-d90c-8ce0-9ae3-5f7-e671.ngrok-free.app";
 export const LOGIN = `${HOST}/api/public/login/admin`;
 export const LIST_WITHDRAWAL = `${HOST}/api/admin/list/withdrawals`;
 export const EXPORT_WITHDRAWAL = `${HOST}/api/admin/export/withdrawals`;
